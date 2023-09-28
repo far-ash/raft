d7babcb139d47f3e0d0c89b5c37e7f95df1d2ea2
Farzin Ashraf MA <farzinashraf1000@gmail.com>
2023-09-28 20:37:51 +0530

    Changed most of my mistakes

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -13,22 +13,22 @@ pub struct Cli {
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
-    /// Initializes a Crafty project in the current directory or the specified path.
+    /// Initializes a C (Raft) project in the current directory or the specified path.
     ///
-    /// The `crafty init` command is used to set up a new Crafty project. If no path is provided,
+    /// The `Raft init` command is used to set up a new Raft project. If no path is provided,
     /// it initializes the project in the current working directory. Alternatively, you can specify
     /// a custom path to create the project in a different directory.
     ///
     /// Examples:
     ///
-    ///     Initialize a Crafty project in the current directory:
-    ///     $ crafty init
+    ///     Initialize a Raft project in the current directory:
+    ///     $ raft init
     ///
-    ///     Initialize a Crafty project in a custom directory:
-    ///     $ crafty init /path/to/my_project
+    ///     Initialize a Raft project in a custom directory:
+    ///     $ raft init /path/to/my/project
     ///
     /// This command will create the necessary project structure and configuration files to get
-    /// started with your C/C++ project using Crafty.
+    /// started with your C/C++ project using Raft.
     Init(InitArgs),
 
     Build(BuildArgs),
